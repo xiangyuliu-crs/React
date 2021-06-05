514b3a8d96785691d8f7bf24b600c216abe77f35
Xiangyu Liu <xiangyu.liu@crssoftware.co.nz>
2021-06-06 10:59:40 +1200

    add CounterHookExample

---
+++ src/CounterHookExample.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+export function CounterHookExample() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
