c41aabb99b21ea9e11aeda4e1f80e25e4b637a73
Xiangyu Liu <xiangyu.liu@crssoftware.co.nz>
2021-06-06 11:31:15 +1200

    add an input field into the hook example

--- src/CounterHookExample.js
+++ src/CounterHookExample.js
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from "react";
 
 export function CounterHookExample() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
+    document.title = `${name} clicked ${count} times`;
   });
 
   return (
     <div>
+      <p>{name} Well done!</p>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
+      <p>Input your name</p>
+      <input value={name} onChange={(event) => setName(event.target.value)} />
     </div>
   );
 }
