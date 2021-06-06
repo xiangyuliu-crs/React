8f7c2163d778f9fcbc8f28ff308b0524aee5217f
Xiangyu Liu <xiangyu.liu@crssoftware.co.nz>
2021-06-06 17:48:21 +1200

    add CounterComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { CounterHookExample } from "./CounterHookExample";
+import { CounterComponent } from "./CounterComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <CounterHookExample />
+        <CounterComponent />
       </header>
     </div>
   );
---
+++ src/CounterComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export class CounterComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is CounterComponent: You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          +
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+          -
+        </button>
+      </div>
+    );
+  }
+}
